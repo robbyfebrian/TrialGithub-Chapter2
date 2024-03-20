f9b77bc767dfa0d936e2044e16de70c40ab4b122
robby febrian <robbyfebrian77@gmail.com>
2024-03-20 22:36:23 +0700

    Membuat file css dan menghubungkan dengan index.html lalu memperbaiki script ber-id jebakan

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById('trial').innerHTML = "Ini adalah text hasil script"
 
-document.getElementById('suprise').innerHTML = "Ambatukam Country"
+document.getElementById('jebakan').innerHTML = "Ambatukam Country"
